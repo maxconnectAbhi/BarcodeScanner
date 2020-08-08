46ab811e2dd5c71454aef081171dfaf10e789ad0
INTEC INFONET <abhi.maxconnect@gmail.com>
2020-08-08 17:55:17 +0530

    task complete

--- BarcodeScanner/App.js
+++ BarcodeScanner/App.js
@@ -1,114 +1,79 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+Text,
+View,
+StyleSheet,
+Alert,
+TouchableOpacity,
+Image
 } from 'react-native';
+import {RNCamera} from 'react-native-camera';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+export default class BarcodeScan extends Component {
+constructor(props) {
+    console.disableYellowBox = true;
+    super(props);
+this.state = {
+    openScanner: false,
+    scan:true
+}
+}
 
+onBarCodeRead = (e) => {
+    this.setState({scan:false})
+Alert.alert(
+    "Barcode value is " + e.data, "Barcode type is " + e.type, [
+    {text: 'OK', onPress: () => this.setState({scan:true})},
+  ],
+  { cancelable: false }
+)
+}
+
+render() {
+return (
+<View style={styles.container}>
+{this.state.openScanner?
+<RNCamera
+style={styles.preview}
+onBarCodeRead={this.state.scan?this.onBarCodeRead:null}
+ref={cam => this.camera = cam}
+/>
+ : 
+ <Text style={{fontWeight:'bold',textAlign:'center',fontSize:25,}}>Barcode Scanner App</Text>
+ }
+<TouchableOpacity onPress={() => this.setState({openScanner:!this.state.openScanner})} style={styles.bottomOverlay}>
+<Text style={{fontWeight:'bold',textAlign:'center',fontSize:15,color:'#FFF'}}>{this.state.openScanner? 'CLOSE BARCODE SCANNER' : 'OPEN BARCODE SCANNER'}</Text>
+</TouchableOpacity>
+</View>
+)
+}
+
+toggleFlash() {
+    this.setState({
+      flash: flashModeOrder[this.state.flash],
+    });
+  }
+
+}
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+container: {
+flex: 1,justifyContent:'center'
+},
+preview: {
+height:'70%',
+alignItems: 'center'
+},
+cameraIcon: {
+margin: 5,
+height: 40,
+width: 40
+},
+bottomOverlay: {
+  backgroundColor:'blue',
+  padding:8,alignSelf:'center',
+ width:'50%',
+ position:'absolute',bottom:50,
+ borderRadius:6
+},
 });
-
-export default App;
